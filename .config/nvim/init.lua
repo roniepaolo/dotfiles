c1a7d728ece0827dffe73629b958b6ce3563ab48
Ronie Arauco <ronie.arauco@pucp.edu.pe>
2023-11-19 23:01:16 -0500

    Update neovim conf for 2024

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,19 +1,2 @@
-require('roniepaolo.base')
-require('roniepaolo.highlights')
-require('roniepaolo.maps')
-require('roniepaolo.plugins')
-
-local has = vim.fn.has
-local is_mac = has "macunix"
-local is_win = has "win32"
-local is_wsl = has "wsl"
-
-if is_mac == 1 then
-  require('roniepaolo.macos')
-end
-if is_win == 1 then
-  require('roniepaolo.windows')
-end
-if is_wsl == 1 then
-  require('roniepaolo.wsl')
-end
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
